bc3433eb233d7752071b7beddda9023dc373a212
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-07-28 23:25:41 -0300

    Questão 4 foi concluida

--- Questão 4/faturamento.js
+++ Questão 4/faturamento.js
@@ -7,3 +7,21 @@ ES – R$27.165,48
 Outros – R$19.849,53
 
 Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.*/
+
+function porcentagem(estado, total) {
+    return Math.round((100*estado)/total)
+}
+
+sp = 67836.43
+rj = 36678.66
+mg = 29229.88
+es = 27165.48
+outros = 19849.53
+
+total = sp + rj + mg + es + outros
+
+console.log(`Porcentagem do faturamento de SP: ${porcentagem(sp, total)}%`);
+console.log(`Porcentagem do faturamento de RJ: ${porcentagem(rj, total)}%`);
+console.log(`Porcentagem do faturamento de MG: ${porcentagem(mg, total)}%`);
+console.log(`Porcentagem do faturamento de ES: ${porcentagem(es, total)}%`);
+console.log(`Porcentagem do faturamento de outros: ${porcentagem(outros, total)}%`);
